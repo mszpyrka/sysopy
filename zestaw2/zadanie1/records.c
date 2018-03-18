bb57851122efd7d92998d7bc1012647a381eb128
Mateusz Szpyrka <mjsz1701@gmail.com>
2018-03-18 16:13:45 +0100

    Added sorting in sys mode, added comments to code

--- zestaw2/zadanie1/records.c
+++ zestaw2/zadanie1/records.c
@@ -9,6 +9,7 @@
 #include <unistd.h>
 #include <fcntl.h>
 
+// Fills passed array with randomly generated values (in this case -> char values)
 void generate_random_record(char* buffer, int bytes_number) {
 
     for(int i = 0; i < bytes_number; i++)
@@ -24,10 +25,12 @@ void generate(int argc, char** argv) {
         exit(1);
     }
 
+    // Parsing all arguments
     char* filename = argv[2];
     int records_number = atoi(argv[3]);
     int records_length = atoi(argv[4]);
 
+    // Opening file
     FILE* file = fopen(filename, "w");
 
     if(file == NULL) {
@@ -38,6 +41,7 @@ void generate(int argc, char** argv) {
 
     char* buffer = malloc(records_length * sizeof(char));
 
+    // Generating random records and writing them into file
     for(int i = 0; i < records_number; i++) {
 
         generate_random_record(buffer, records_length);
@@ -50,6 +54,7 @@ void generate(int argc, char** argv) {
 
     }
 
+    // Closing file
     if(fclose(file) != 0) {
 
         fprintf(stderr, "error occurred while closing file\n");
@@ -67,7 +72,6 @@ void copy(int argc, char** argv) {
     }
 
     // Parsing all necessary arguments
-
     char* input_filename = argv[2];
     char* output_filename = argv[3];
     int records_number = atoi(argv[4]);
@@ -77,9 +81,9 @@ void copy(int argc, char** argv) {
     char* buffer = malloc(buffer_size * sizeof(char));
 
     // Copying file using C standard library functions
-
     if(strcmp(mode, "lib") == 0) {
 
+        // Opening both input and output files
         FILE* input_file = fopen(input_filename, "r");
         FILE* output_file = fopen(output_filename, "w");
 
@@ -89,6 +93,7 @@ void copy(int argc, char** argv) {
             exit(1);
         }
 
+        // Reading from input and writing to output record by record
         for(int i = 0; i < records_number; i++) {
 
             if(fread(buffer, sizeof(char), buffer_size, input_file) != buffer_size){
@@ -104,6 +109,7 @@ void copy(int argc, char** argv) {
             }
         }
 
+        // Closing files
         if(fclose(input_file) != 0 || fclose(output_file) != 0) {
 
             fprintf(stderr, "error occurred while closing file\n");
@@ -113,12 +119,13 @@ void copy(int argc, char** argv) {
     }
 
     // Copying file using C system call functions
-
     else if(strcmp(mode, "sys") == 0) {
 
+        // Opening input file
         int input_fd = open(input_filename, O_RDONLY);
 
-        // If the output file does not exist it is created with following permissions: owner:rw, group:r, other:r
+        // Opening output file
+        // If the output file does not exist it is created with the following permissions: owner:rw, group:r, other:r
         int output_fd = open(output_filename, O_WRONLY | O_CREAT, S_IRUSR | S_IWUSR | S_IRGRP | S_IROTH);
 
         if(input_fd < 0 || output_fd < 0) {
@@ -127,6 +134,7 @@ void copy(int argc, char** argv) {
             exit(1);
         }
 
+        // Reading from input and writing to output record by record
         for(int i = 0; i < records_number; i++) {
 
             if(read(input_fd, buffer, buffer_size * sizeof(char)) != buffer_size) {
@@ -142,6 +150,7 @@ void copy(int argc, char** argv) {
             }
         }
 
+        // Closing files
         if(close(input_fd) < 0 || close(output_fd) < 0) {
 
             fprintf(stderr, "error occurred while closing file\n");
@@ -150,8 +159,7 @@ void copy(int argc, char** argv) {
 
     }
 
-    // None of both modes was recognized
-
+    // In case none of valid modes was recognized
     else {
 
         fprintf(stderr, "wrong arguments format\n");
@@ -169,19 +177,19 @@ void sort(int argc, char** argv) {
     }
 
     // Parsing all necessary arguments
-
     char* filename = argv[2];
     int records_number = atoi(argv[3]);
     int records_length = atoi(argv[4]);
     char* mode = argv[5];
 
+    // Creating two buffers for storing records in RAM (no more than two at the same time)
     char* uninserted = malloc(records_length * sizeof(char));
     char* swap_buffer = malloc(records_length * sizeof(char));
 
     // Sorting file using C standard library functions
-
     if(strcmp(mode, "lib") == 0) {
 
+        // Opening file
         FILE* file = fopen(filename, "r+");
 
         if(file == NULL) {
@@ -190,36 +198,37 @@ void sort(int argc, char** argv) {
             exit(1);
         }
 
+        // Iterating through all records in file
         for(int i = 1; i < records_number; i++) {
 
+            // Setting file pointer to the beginning of the next record
             int next_record_position = records_length * i;
-
             int target_position = 0;
+            fseek(file, next_record_position * sizeof(char), SEEK_SET);
 
-            fseek(file, next_record_position, SEEK_SET);
+            // Loading next record from file into 'uninserted' array
             fread(uninserted, sizeof(char), records_length, file);
 
-            printf("%d: %s\n", i, uninserted);
-
+            // Searching for a proper position for the record
             for(int j = i; j > 0; j--) {
 
                 int record_iterator = records_length * j;
 
-                fseek(file, record_iterator - records_length, SEEK_SET);
+                // Loading record from file into 'swap_buffer' array to change its position in file
+                fseek(file, (record_iterator - records_length) * sizeof(char), SEEK_SET);
                 fread(swap_buffer, sizeof(char), records_length, file);
 
-                printf("%s\n", swap_buffer);
-
+                // If proper position is found -> ends the inner loop
                 if(uninserted[0] > swap_buffer[0]) {
 
-                    // Proper position for next record is found
                     target_position = record_iterator;
                     break;
                 }
 
+                // If not -> rewrites previously read record to other place in file
                 else {
 
-                    fseek(file, record_iterator, SEEK_SET);
+                    fseek(file, record_iterator * sizeof(char), SEEK_SET);
 
                     if(fwrite(swap_buffer, sizeof(char), records_length, file) != records_length){
 
@@ -229,16 +238,19 @@ void sort(int argc, char** argv) {
                 }
             }
 
-            // Record is inserted into its proper position in file
-            fseek(file, target_position, SEEK_SET);
+            // Inserting record into its proper position in file after finishing inner loop
+            fseek(file, target_position * sizeof(char), SEEK_SET);
             fwrite(uninserted, sizeof(char), records_length, file);
         }
 
+        // Closing file
         fclose(file);
     }
 
+    // Sorting with C system call functions
     else if(strcmp(mode, "sys") == 0) {
 
+        // Opening file
         int file_desc = open(filename, O_RDWR);
 
         if(file_desc < 0) {
@@ -247,60 +259,37 @@ void sort(int argc, char** argv) {
             exit(1);
         }
 
+        // Iterating through all records in file
         for(int i = 1; i < records_number; i++) {
 
+            // Setting file pointer to the beginning of the next record
             int next_record_position = records_length * i;
+            int target_position = 0;
+            lseek(file_desc, next_record_position * sizeof(char), SEEK_SET);
 
-            if(lseek(file_desc, next_record_position, SEEK_SET) != 0) {
+            // Loading next record from file into 'uninserted' array
+            read(file_desc, uninserted, records_length * sizeof(char));
 
-                fprintf(stderr, "error occurred while setting file position\n");
-                exit(1);
-            }
+            // Searching for a proper position for the record
+            for(int j = i; j > 0; j--) {
 
-            if(read(file_desc, uninserted, records_length * sizeof(char)) != records_length){
+                int record_iterator = records_length * j;
 
-                fprintf(stderr, "error occurred while reading from file\n");
-                exit(1);
-            }
+                // Loading record from file into 'swap_buffer' array to change its position in file
+                lseek(file_desc, (record_iterator - records_length) * sizeof(char), SEEK_SET);
+                read(file_desc, swap_buffer, records_length * sizeof(char));
 
-            for(int record_iterator = next_record_position; record_iterator > 0; record_iterator -= records_length) {
-
-                if(lseek(file_desc, record_iterator - records_length, SEEK_SET) != 0) {
-
-                    fprintf(stderr, "error occurred while setting file position\n");
-                    exit(1);
-                }
-
-                if(read(file_desc, swap_buffer, records_length * sizeof(char)) != records_length){
-
-                    fprintf(stderr, "error occurred while reading from file\n");
-                    exit(1);
-                }
-
+                // If proper position is found -> ends the inner loop
                 if(uninserted[0] > swap_buffer[0]) {
 
-                    if(lseek(file_desc, record_iterator, SEEK_SET) != 0) {
-
-                        fprintf(stderr, "error occurred while setting file position\n");
-                        exit(1);
-                    }
-
-                    if(write(file_desc, uninserted, records_length * sizeof(char)) != records_length){
-
-                        fprintf(stderr, "error occurred while writing into file\n");
-                        exit(1);
-                    }
-
+                    target_position = record_iterator;
                     break;
                 }
 
+                // If not -> rewrites previously read record to other place in file
                 else {
 
-                    if(lseek(file_desc, record_iterator, SEEK_SET) != 0) {
-
-                        fprintf(stderr, "error occurred while setting file position\n");
-                        exit(1);
-                    }
+                    lseek(file_desc, record_iterator * sizeof(char), SEEK_SET);
 
                     if(write(file_desc, swap_buffer, records_length * sizeof(char)) != records_length){
 
@@ -309,7 +298,14 @@ void sort(int argc, char** argv) {
                     }
                 }
             }
+
+            // Inserting record into its proper position in file after finishing inner loop
+            lseek(file_desc, target_position * sizeof(char), SEEK_SET);
+            write(file_desc, uninserted, records_length * sizeof(char));
         }
+
+        // Closing file
+        close(file_desc);
     }
 
     // None of both modes was recognized
