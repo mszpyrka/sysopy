0ea2fcf3f7ceb1cb85a9d56d95bf9b0f4b21132c
Mateusz Szpyrka <mjsz1701@gmail.com>
2018-03-24 15:40:31 +0100

    Fixed resource usage report text color

--- zestaw3/zad3/interpreter2.c
+++ zestaw3/zad3/interpreter2.c
@@ -12,13 +12,9 @@
 #include <ctype.h>
 #include <inttypes.h>
 
-#define ANSI_COLOR_RED     "\x1b[31m"
-#define ANSI_COLOR_GREEN   "\x1b[32m"
-#define ANSI_COLOR_YELLOW  "\x1b[33m"
-#define ANSI_COLOR_BLUE    "\x1b[34m"
-#define ANSI_COLOR_MAGENTA "\x1b[35m"
-#define ANSI_COLOR_CYAN    "\x1b[36m"
-#define ANSI_COLOR_RESET   "\x1b[0m"
+#define ANSI_COLOR_RED     "\033[31m"
+#define ANSI_COLOR_GREEN   "\033[32m"
+#define ANSI_COLOR_RESET   "\033[0m"
 
 #define ARGS_LIMIT 15
 int line_counter = 0; // Stores number of currently processed line in batch file
@@ -157,6 +153,7 @@ void process_next_line(char* buffer, struct rlimit* cpu_limit, struct rlimit* me
     printf("mem:\t");
     printf("\n");/// TODO - memory usage calculating (???)
     printf(ANSI_COLOR_RESET);
+    fflush(stdout);
 
 }
 
