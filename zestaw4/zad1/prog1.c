283b202a564d4ab069025cc13e556806ee133d80
Mateusz Szpyrka <mjsz1701@gmail.com>
2018-04-02 23:39:47 +0200

    Completed task 4.1.1

--- zestaw4/zad1/prog1.c
+++ zestaw4/zad1/prog1.c
@@ -5,7 +5,13 @@
 #include <unistd.h>
 #include <signal.h>
 
+
+// Flag that stores the current program mode (whether it should be running or waiting for SIGTSTP to continue)
 int main_loop_active;
+
+// Buffer storing time string
+char buffer[50];
+
 
 // Prints current time to stdio
 void print_time() {
@@ -14,7 +20,8 @@ void print_time() {
     time(&int_time);
     struct tm *current_time = localtime(&int_time);
 
-    printf("%s\n", asctime(current_time));
+    strftime(buffer, 50, "%H:%M:%S", current_time);
+    printf("time: %s\n", buffer);
     fflush(stdout); // To ensure that the date is printed at this exact point of program execution
 }
 
@@ -26,17 +33,56 @@ void sig_int(int signo) {
     exit(0);
 }
 
+
+// Function used for handling SIGTSTP
+void sig_tstp(int signo) {
+
+    if(main_loop_active == 1) {
+
+        main_loop_active = 0;
+        printf("Oczekuje na CTRL+Z - kontynuacja albo CTRL+C - zakonczenie programu\n");
+        fflush(stdout);
+    }
+
+    else
+        main_loop_active = 1;
+}
+
+
 int main(int argc, char** argv) {
 
+    // Setting handler for SIGINT
     if(signal(SIGINT, sig_int) == SIG_ERR) {
 
         perror("Cannot set SIGINT signal handler");
         exit(1);
     }
 
+    // struct sigaction setup
+    struct sigaction act_tstp;
+    act_tstp.sa_handler = sig_tstp;
+    sigemptyset(&act_tstp.sa_mask);
+    act_tstp.sa_flags = 0;
+
+    // Setting handler for SIGTSTP
+    if(sigaction(SIGTSTP, &act_tstp, NULL) == -1) {
+
+        perror("Cannot set SIGTSTP signal handler");
+        exit(1);
+    }
+
+    main_loop_active = 1;
+
+    // Infinite loop
     while(1) {
 
-        print_time();
-        sleep(1);
+        if(main_loop_active == 1) {
+
+            print_time();
+            sleep(1);
+        }
+
+        else
+            pause();
     }
 }
